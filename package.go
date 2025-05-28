29e3b04e8dbfed5bac91047c865d32ec0817a7ae
weiclw <92598652+weiclw@users.noreply.github.com>
2025-05-27 17:44:32 -0700

    Make it built

--- package.go
+++ package.go
@@ -7,11 +7,33 @@ import (
 )
 
 func getAllDirs() []string {
-	return []string{
+	home, err := os.UserHomeDir()
+	if err != nil {
+		fmt.Printf("Failed to get home dir: %v\n", err)
+		return []string{}
+	}
+
+	intermediates := []string{
+		"Application",
+	}
+
+	app_dir := home
+	for _, part := range intermediates {
+		app_dir = filepath.Join(app_dir, part)
+	}
+
+	relative_list := []string{
 		"Contents",
 		"Contents/MacOS",
 		"Contents/Resources",
 	}
+
+	ret := []string{}
+	for _, p := range relative_list {
+		ret = append(ret, filepath.Join(app_dir, p))
+	}
+
+	return ret
 }
 
 // genPackage creates a directory and writes "hello" into a file
@@ -24,7 +46,13 @@ func genPackage(browser, app string) {
 	}
 
 	// Creating all directories in the wrapper package.
-	for _, path := range getAllDirs() {
+        all_dirs := getAllDirs()
+	if len(all_dirs) == 0 {
+		fmt.Printf("Fails to get directory list")
+		return
+	}
+
+	for _, path := range all_dirs {
 		if err := os.MkdirAll(path, 0755); err != nil {
 			fmt.Printf("Failed to create dir: %v\n", err)
 			return
