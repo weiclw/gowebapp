7f0704e383a2e534a561f54cf72a9dedf039760a
weiclw <92598652+weiclw@users.noreply.github.com>
2025-05-27 17:30:53 -0700

    Add list of dirs

--- package.go
+++ package.go
@@ -6,6 +6,14 @@ import (
 	"path/filepath"
 )
 
+func getAllDirs() []string {
+	return []string{
+		"Contents",
+		"Contents/MacOS",
+		"Contents/Resources",
+	}
+}
+
 // genPackage creates a directory and writes "hello" into a file
 func genPackage(browser, app string) {
 	dir := filepath.Join("foo", "bar")
@@ -13,6 +21,14 @@ func genPackage(browser, app string) {
 	if err := os.MkdirAll(dir, 0755); err != nil {
 		fmt.Printf("Failed to create directory: %v\n", err)
 		return
+	}
+
+	// Creating all directories in the wrapper package.
+	for _, path := range getAllDirs() {
+		if err := os.MkdirAll(path, 0755); err != nil {
+			fmt.Printf("Failed to create dir: %v\n", err)
+			return
+		}
 	}
 
 	filePath := filepath.Join(dir, "f1")
